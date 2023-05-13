c858b651aa40b20a6605184941eb15d11d415b3f
Jose Contreras <ji.ccdev@outlook.com>
2023-05-12 23:47:51 -0400

    feat: optimize pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Pagination = ({ properties }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
-  const propertiesPerPage = 10;
+  const propertiesPerPage = 5;
   const totalPages = Math.ceil(properties.length / propertiesPerPage);
 
   const startIndex = (currentPage - 1) * propertiesPerPage;
@@ -27,38 +27,55 @@ const Pagination = ({ properties }) => {
   };
 
   const getPageNumbers = () => {
-    const pageNumbers = [];
-
-    if (totalPages <= 5) {
-      for (let i = 1; i <= totalPages; i++) {
-        pageNumbers.push(i);
-      }
-    } else {
-      if (currentPage <= 3) {
-        for (let i = 1; i <= 5; i++) {
-          pageNumbers.push(i);
-        }
-        pageNumbers.push('...');
-        pageNumbers.push(totalPages);
-      } else if (currentPage >= totalPages - 2) {
-        pageNumbers.push(1);
-        pageNumbers.push('...');
-        for (let i = totalPages - 4; i <= totalPages; i++) {
-          pageNumbers.push(i);
-        }
-      } else {
-        pageNumbers.push(1);
-        pageNumbers.push('...');
-        for (let i = currentPage - 1; i <= currentPage + 1; i++) {
-          pageNumbers.push(i);
-        }
-        pageNumbers.push('...');
-        pageNumbers.push(totalPages);
-      }
-    }
+    const pageNumbers =
+      totalPages <= 5
+        ? Array.from({ length: totalPages }, (_, i) => i + 1)
+        : [
+            1,
+            currentPage <= 3 || currentPage >= totalPages - 2 ? '...' : null,
+            ...Array.from({ length: 3 }, (_, i) => currentPage - 1 + i).filter(
+              (pageNumber) => pageNumber > 1 && pageNumber < totalPages
+            ),
+            currentPage >= totalPages - 2 ? '...' : null,
+            totalPages,
+          ].filter(Boolean);
 
     return pageNumbers;
   };
+
+  // const getPageNumbers = () => {
+  //   const pageNumbers = [];
+
+  //   if (totalPages <= 5) {
+  //     for (let i = 1; i <= totalPages; i++) {
+  //       pageNumbers.push(i);
+  //     }
+  //   } else {
+  //     if (currentPage <= 3) {
+  //       for (let i = 1; i <= 5; i++) {
+  //         pageNumbers.push(i);
+  //       }
+  //       pageNumbers.push('...');
+  //       pageNumbers.push(totalPages);
+  //     } else if (currentPage >= totalPages - 2) {
+  //       pageNumbers.push(1);
+  //       pageNumbers.push('...');
+  //       for (let i = totalPages - 4; i <= totalPages; i++) {
+  //         pageNumbers.push(i);
+  //       }
+  //     } else {
+  //       pageNumbers.push(1);
+  //       pageNumbers.push('...');
+  //       for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+  //         pageNumbers.push(i);
+  //       }
+  //       pageNumbers.push('...');
+  //       pageNumbers.push(totalPages);
+  //     }
+  //   }
+
+  //   return pageNumbers;
+  // };
 
   return (
     <div>
