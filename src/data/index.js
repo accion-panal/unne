422a11d76b4f887df63a53ee4960252b0e32aa77
ManuelMontenegroVerdugo <m.montenegro.verdugo@gmail.com>
2023-05-23 12:27:29 -0400

    feature:Auctions

--- src/data/index.js
+++ src/data/index.js
@@ -1,3 +1,13 @@
+import Slide1 from '../assets/img/carousel/slide1.jpg';
+import Slide4 from '../assets/img/carousel/slide4.jpg';
+
+
+
+
+
+
+
+
 /** Main Carousel Data */
 export const mainCarouselData = [
   {
@@ -16,3 +26,205 @@ export const mainCarouselData = [
 
 /** Tabs search by Property Data */
 export const webServicesTabs = ['venta', 'arriendo ', 'inversion'];
+
+
+
+
+/** porque unne - Arrendar Data */
+export const whyUnneLeaseData = [
+  {
+    id: 1,
+    title: '¿Por qué Unne?',
+    benefits: [
+      {
+        id: 1,
+        name: 'Te acompañamos en toda la gestión.',
+        icon: 'before:content-["1"]',
+      },
+      {
+        id: 2,
+        name: 'Tu propiedad en manos del arrendatario correcto.',
+        icon: 'before:content-["2"]',
+      },
+      {
+        id: 3,
+        name: 'Publicamos en los portales mas reconocidos de Chile.',
+        icon: 'before:content-["3"]',
+      },
+      {
+        id: 4,
+        name: 'Asesoria Legal.',
+        icon: 'before:content-["4"]',
+      },
+      {
+        id: 5,
+        name: 'Agenda tu sesión de fotos y video profesional de manera gratuita.',
+        icon: 'before:content-["5"]',
+      },
+    ],
+  },
+];
+
+
+
+/* Informacion del circulo en quiero-arrendar.js */
+export const CircleArrendarData = [
+  {
+    id: 1,
+    text: 'Llena el formulario',
+    position: 'lg:top-0 lg:right-[19rem] md:max-lg:left-2/4 md:-top-8',
+  },
+  {
+    id: 2,
+    text: 'Te asignamos a tu objetivo',
+    position: 'lg:right-full md:max-lg:top-10 md:max-lg:left-[24rem]',
+  },
+  {
+    id: 3,
+    text: 'Evaluacion Comercial',
+    position:
+      'lg:right-[19rem] lg:bottom-0 md:max-lg:top-[9rem] md:max-lg:left-[28rem] ',
+  },
+  {
+    id: 4,
+    text: 'Definicion de estrategias',
+    position:
+      'lg:bottom-0 lg:left-[19rem] md:max-lg:left-2/4 md:max-lg:-bottom-8 ',
+  },
+  {
+    id: 5,
+    text: 'Gestion Comercial',
+    position: 'lg:left-full md:max-lg:bottom-10 md:max-lg:left-[24rem]',
+  },
+  {
+    id: 6,
+    text: '¡Arrendamos y recibe tu arriendo!',
+    position:
+      'lg:top-0 lg:left-[19rem] md:max-lg:bottom-[9rem] md:max-lg:left-[28rem] ',
+  },
+];
+
+
+
+/* Informacion del circulo en quiero-vender.js */
+export const CircleVenderData = [
+  {
+    id: 1,
+    text: 'Llena el formulario',
+    position: 'lg:top-0 lg:right-[19rem] md:max-lg:left-2/4 md:-top-8',
+  },
+  {
+    id: 2,
+    text: 'Te asignamos a tu Ejecutivo',
+    position: 'lg:right-full md:max-lg:top-10 md:max-lg:left-[24rem]',
+  },
+  {
+    id: 3,
+    text: 'Evaluación Comercial',
+    position:
+      'lg:right-[19rem] lg:bottom-0 md:max-lg:top-[9rem] md:max-lg:left-[28rem]',
+  },
+  {
+    id: 4,
+    text: 'Definicion de estrategias',
+    position:
+      'lg:bottom-0 lg:left-[19rem] md:max-lg:left-2/4 md:max-lg:-bottom-8',
+  },
+  {
+    id: 5,
+    text: 'Gestión Comercial y legal',
+    position: 'lg:left-full md:max-lg:bottom-10 md:max-lg:left-[24rem]',
+  },
+  {
+    id: 6,
+    text: 'vende y recibe tu pago!',
+    position:
+      'lg:top-0 lg:left-[19rem] md:max-lg:bottom-[9rem] md:max-lg:left-[28rem]',
+  },
+];
+
+
+
+/** Main Carousel - Vender Data */
+export const mainCarouselSellData = [
+  {
+    id: 1,
+    title: 'Vende fácil y rápido',
+    desc: '',
+    img: Slide1,
+  },
+  {
+    id: 2,
+    title: 'Vende fácil y rápido',
+    desc: '',
+    img: Slide4,
+  },
+];
+
+
+/** porque unne - vender Data */
+export const whyUnneSellData = [
+  {
+    id: 1,
+    title: '¿Por qué Unne?',
+    benefits: [
+      {
+        id: 1,
+        name: 'Te asesoramos de principio a fin.',
+        icon: 'before:content-["1"]',
+      },
+      {
+        id: 1,
+        name: 'Tenemos la base  de clientes compradores más grande del país.',
+        icon: 'before:content-["2"]',
+      },
+      {
+        id: 1,
+        name: 'Publicamos en 43 portales inmobiliarios Servicios legales.',
+        icon: 'before:content-["3"]',
+      },
+      {
+        id: 1,
+        name: 'Servicios legales.',
+        icon: 'before:content-["4"]',
+      },
+      {
+        id: 1,
+        name: 'Agenda tu sesión de fotos y video profesional de manera gratuita.',
+        icon: 'before:content-["5"]',
+      },
+    ],
+  },
+];
+
+
+
+
+/** Objetives Auction page icons,title,desc */
+export const ObjetivesAuctionData = [
+  {
+    id: 1,
+    icon: 'A',
+    title: 'Defenición de objetivos y necesidades',
+    desc: 'Establecemos el capital que dispones para tu inversión mediante una reunión de trabajo',
+  },
+  {
+    id: 2,
+    icon: 'B',
+    title: 'Nos encargamos de todo el proceso',
+    desc: 'Legal, pago de servicios básicos e impuestos sobre la renta, reparaciones o remodelaciòn de la propiedad',
+  },
+  {
+    id: 3,
+    icon: 'C',
+    title: 'Buscamos la mejor propiedad en remate para ti',
+    desc: 'Se realiza el remate y se adjudica el inmueble para posteriormente pactar la rentabilidad que generara.',
+  },
+
+  {
+    id: 4,
+    icon: 'D',
+    title: 'El dinero retorna con la rentabilidad pactada',
+    desc: 'Obtén las ganancias esperadas por tu inversión',
+  },
+];
