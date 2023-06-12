8afec937049fe995ca5bada6caacc98f9a3dd8c5
ManuelMontenegroVerdugo <m.montenegro.verdugo@gmail.com>
2023-06-12 16:06:42 -0400

    feature: Responsives Foms and components

--- src/components/PageSections/Owner/ToLease/WhyUnneList.jsx
+++ src/components/PageSections/Owner/ToLease/WhyUnneList.jsx
@@ -5,7 +5,7 @@ const WhyUnneList = ({ data, subFont }) => {
     const { id, name, icon } = data;
     return (
         <div className="">
-            <li className=' list-none relative max-[333px]:w-[250px] w-[400px] sm:w-[450px] lg:w-[480px] min-w-full max-w-[45rem] pt-6 pb-2 px-8 hover:shadow-2xl shadow-xl rounded-2xl overflow-hidden bg-white min-h-[120px]
+            <li className=' list-none relative max-[333px]:w-[250px] w-[300px] sm:w-[450px] lg:w-[480px] min-w-full max-w-[45rem] pt-6 pb-2 px-8 hover:shadow-2xl shadow-xl rounded-2xl overflow-hidden bg-white min-h-[120px]
                 before:content-[""] before:block before:w-full before:h-4 before:absolute before:top-0 before:left-0 before:bg-[#FAB916]'>
                 <h3 className={`w-full text-center align-baseline ${subFont} font-bold m-0 text-[#fb923c]`}>
                     {name}
